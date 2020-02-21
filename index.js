66f61596f4808240cf77691daa8419a8a7659415
Dominique Skaf <doskaf@yahoo.com>
2020-02-21 15:55:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -37,5 +37,5 @@ function unleashDog(dogName, dogBreed) {
 let routine = [wakeDog(), leashDog(), walkToPark(), throwFrisbee(), walkHome(), unleashDog()];
 
 function exerciseDog(dogName, dogBreed) {
-  
+  for (let i = 0; i < routine.length; i++)
 }
