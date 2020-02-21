22856e0af97c866edff500674597d2d21903e7ae
Dominique Skaf <doskaf@yahoo.com>
2020-02-21 15:52:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ function wakeDog(dogName, dogBreed) {
 }
 
 function leashDog(dogName, dogBreed) {
+  let string = `Leash ${dogName} the ${dogBreed}`;
+  console.log(string);
+  return string;
+}
+
+function walkToPark(dogName, dogBreed) {
   let string = `Wake ${dogName} the ${dogBreed}`;
   console.log(string);
   return string;
